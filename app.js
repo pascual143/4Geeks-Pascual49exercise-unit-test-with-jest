d2f37dba5ba6891da62f12afa828241c435635e7
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-10-23 19:32:38 +0000

    final

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-// // Esta es mi función que suma dos números
- const sum = (a,b) => {
-     return a + b
- }
- 
- // Solo un registro en consola para nosotros
- console.log(sum(7,3))
- 
- // Exporta la función para usarla en otros archivos 
- // (similar a la palabra clave "export" cuando se usa webpack)
-module.exports = { sum };
-
-// one euro is:
 let oneEuroIs = {
     "JPY": 156.5, // japan yen
     "USD": 1.07, // us dollar
     "GBP": 0.87, // british pound
 }
+
+// Esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// Solo un registro en consola para nosotros
+console.log(sum(7,3))
+
+// Exporta la función para usarla en otros archivos 
+// (similar a la palabra clave "export" cuando se usa webpack)
+module.exports = { sum };
+
+//Ejercicios de conversion de moneda
 
 // Declaramos una función con el nombre exacto "formEuroToDollar"
 const fromEuroToDollar = function(valueInEuro) {
@@ -25,5 +26,21 @@ const fromEuroToDollar = function(valueInEuro) {
     return valueInDollar;
 }
 
+// Declaramos una función con el nombre exacto "formDollarToYen"
+const fromDollarToYen = function(valueInDollar) {
+    // Convertimos el valor a dólares
+    let valueInYen = valueInDollar * oneEuroIs.JPY / oneEuroIs.USD;
+    // Retornamos el valor en dólares
+    return valueInYen;
+}
+
+// Declaramos una función con el nombre exacto "formYenToGbp"
+const fromYenToPound = function(valueInYen) {
+    // Convertimos el valor a dólares
+    let valueInGbp = valueInYen * oneEuroIs.GBP / oneEuroIs.JPY
+    // Retornamos el valor en dólares
+    return valueInGbp;
+}
+
 // Tenemos que incluir la función en el exports para que sea exportada a otros archivos como test.js
-module.exports = { sum, fromEuroToDollar }
+module.exports = { sum, fromEuroToDollar, fromYenToPound, fromDollarToYen  }
--- test.js
+++ test.js
@@ -2,13 +2,14 @@
 const { sum } = require('./app.js');
 
 // Comienza tu primera prueba
- test('adds 14 + 9 to equal 23', () => {
-     // Dentro de la prueba llamamos a nuestra función sum con 2 números
-     let total = sum(14, 9);
- 
-     // Esperamos que la suma de esos 2 números sea 23
-     expect(total).toBe(23);
- });
+test('la suma de 14 + 9 es igual 23', () => {
+    // Dentro de la prueba llamamos a nuestra función sum con 2 números
+    let total = sum(14, 9);
+
+    // Esperamos que la suma de esos 2 números sea 23
+    expect(total).toBe(23);
+});
+//Ejercicios de conversion de monedas
 
 test("One euro should be 1.07 dollars", function() {
     // Importo la funcion desde app.js
@@ -22,4 +23,24 @@ test("One euro should be 1.07 dollars", function() {
 
     // Hago mi comparación (la prueba)
     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+})
+
+test("One euro should be 156.5 yens", function() {
+    const { fromDollarToYen } = require('./app.js');
+
+    const yens = fromDollarToYen(1.07);
+
+    const expected = 1.07 * 156.6 / 1.07;
+
+    expect(fromDollarToYen(1.07)).toBe(15.66); 
+})
+
+test("One euro should be 0.87 Gbp", function() {
+    const { fromYenToPound } = require('./app.js');
+
+    const Gbp = fromYenToPound(156.5);
+
+    const expected = 156.5 * 0.87 / 156.5;
+
+    expect(fromYenToPound(156.5)).toBe(0.87); 
 })
