648eba0238a9188fe182abeb3997b77f2e5757d1
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-11-06 08:36:36 +0000

    fixed

--- test.js
+++ test.js
@@ -25,14 +25,14 @@ test("One euro should be 1.07 dollars", function() {
     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 })
 
-test("One euro should be 156.5 yens", function() {
+test("One euro should be 15.66 yens", function() {
     const { fromDollarToYen } = require('./app.js');
 
     const yens = fromDollarToYen(1.07);
 
-    const expected = 1.07 * 156.6 / 1.07;
+    const expected = 1.07 * 156.5 / 1.07;
 
-    expect(fromDollarToYen(1.07)).toBe(15.66); 
+    expect(fromDollarToYen(1.07)).toBe(156.5);
 })
 
 test("One euro should be 0.87 Gbp", function() {
